7fc7982a8fcc90b688331b5395e882218b4bf0cd
Rishabh Jain <Rishabh9843@gmail.com>
2020-07-28 16:24:03 +0530

    commit

--- corona.py
+++ corona.py
@@ -10,7 +10,7 @@ numbers = soup.find_all('div', {"class": "maincounter-number"})
 total_cases = numbers[0].span.text
 deaths = numbers[1].span.text
 recovered = numbers[2].span.text
-
+print("World Status")
 print("Total cases: ", total_cases)
 print("Deaths: ", deaths)
 print("Recovered: ", recovered)
